1d9fb30909ab7b3f11ab8c60dc1673e79bed004a
sainRaghuveer <raghuveersain987@gmail.com>
2023-05-28 20:17:22 +0530

    toast

--- cognisite/src/pages/Login.jsx
+++ cognisite/src/pages/Login.jsx
@@ -1,12 +1,15 @@
-import { Box, Button, FormControl, Input } from '@chakra-ui/react';
+import { Box, Button, FormControl, Input, useToast } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from "../styles/Signup.module.css"
+import styles from "../styles/Signup.module.css";
+import { Spinner } from '@chakra-ui/react';
 
 const Login = () => {
     const [mobile, setMobile] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const toast = useToast();
 
 
     const handleSubmit = async (e) => {
@@ -19,25 +22,54 @@ const Login = () => {
 
         // Send the login credentials to the server for authentication
         try {
+            setLoading(true);
             const response = await fetch('https://mock-serverfor-mock6.onrender.com/users');
             if (response.ok) {
+                setLoading(false);
                 const users = await response.json();
                 const isRegistered = users.some((u) => u.mobile === mobile && u.password === password);
                 if (isRegistered) {
                     sessionStorage.setItem("user", mobile)
                     console.log('User Logged in successfully');
                     navigate("/home");
+                    toast({
+                        title: `User Logged in successfully`,
+                        status: "success",
+                        isClosable: true,
+                        position: "top"
+                    });
                     // Reset the form fields
                     setMobile('');
                     setPassword('');
                     return;
+                }else{
+                    toast({
+                        title: `Check your password or phone number`,
+                        status: "error",
+                        isClosable: true,
+                        position: "top"
+                    });
                 }
             } else {
                 console.log('Failed to fetch users');
+                toast({
+                    title: `Failed to fetch users`,
+                    status: "error",
+                    isClosable: true,
+                    position: "top"
+                });
+                setLoading(false);
             }
         } catch (error) {
             console.log('Error:', error);
+            toast({
+                title: `${error}`,
+                status: "error",
+                isClosable: true,
+                position: "top"
+            });
         }
+        setLoading(false);
     };
 
     return (
@@ -54,7 +86,7 @@ const Login = () => {
                 </label>
                 <br />
                 <Box style={{ width: "50%", margin: "auto", textAlign: "center", marginTop: "50px" }}>
-                    <Button style={{backgroundColor:"teal"}} className={styles.btn} type="submit">Login</Button>
+                    <Button style={{backgroundColor:"teal"}} className={styles.btn} type="submit">{loading?<Spinner/>:"Login"}</Button>
                 </Box>
             </form>
         </div>
--- cognisite/src/pages/Signup.jsx
+++ cognisite/src/pages/Signup.jsx
@@ -1,13 +1,16 @@
-import { Box, Button, Input, Text } from '@chakra-ui/react';
+import { Box, Button, Input, Text, useToast } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from "../styles/Signup.module.css"
+import styles from "../styles/Signup.module.css";
+import { Spinner } from '@chakra-ui/react';
 
 const Signup = () => {
     const [name, setName] = useState('');
     const [mobile, setMobile] = useState('');
     const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const toast =useToast();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -20,13 +23,21 @@ const Signup = () => {
         console.log(user);
 
         try {
+            setLoading(true);
             const response = await fetch('https://mock-serverfor-mock6.onrender.com/users');
+            setLoading(false);
             if (response.ok) {
                 const users = await response.json();
                 const isRegistered = users.some((u) => u.mobile === mobile);
                 if (isRegistered) {
                     console.log('User is already registered');
                     // Reset the form fields
+                    toast({
+                        title: `User is already registered please login`,
+                        status: "warning",
+                        isClosable: true,
+                        position: "top"
+                    });
                     setName('');
                     setMobile('');
                     setPassword('');
@@ -34,13 +45,16 @@ const Signup = () => {
                 }
             } else {
                 console.log('Failed to fetch users');
+                setLoading(false);
             }
         } catch (error) {
             console.log('Error:', error);
+            setLoading(false);
         }
         // Send the user data to the server and store it in the JSON file
 
         try {
+            setLoading(true);
             const response = await fetch('https://mock-serverfor-mock6.onrender.com/users', {
                 method: 'POST',
                 headers: {
@@ -50,18 +64,38 @@ const Signup = () => {
             });
 
             console.log("signupres", response)
+            setLoading(false);
             if (response.ok) {
                 console.log('User registered successfully');
                 navigate("/login");
+                toast({
+                    title: `User registered successfully`,
+                    status: "success",
+                    isClosable: true,
+                    position: "top"
+                });
                 // Reset the form fields
                 setName('');
                 setMobile('');
                 setPassword('');
             } else {
                 console.log('Failed to register user');
+                toast({
+                    title: `Failed to register user`,
+                    status: "error",
+                    isClosable: true,
+                    position: "top"
+                });
             }
         } catch (error) {
             console.log('Error:', error);
+            toast({
+                title: `${error}`,
+                status: "error",
+                isClosable: true,
+                position: "top"
+            })
+            setLoading(false);
         }
     };
 
@@ -86,11 +120,11 @@ const Signup = () => {
                 </label>
                 <br />
                 <Box style={{width:"50%", margin:"auto", textAlign:"center", marginTop:"50px"}}>
-                <Button style={{backgroundColor:"teal"}} type="submit">Register</Button>
+                <Button style={{backgroundColor:"teal"}} type="submit">{loading?<Spinner/>:"Register"}</Button>
                 </Box>
                 <Box style={{ display: "flex", width: "80%", margin: "auto", justifyContent: "space-around", marginTop: "30px" }}>
                     <Text style={{marginTop:"7px"}}>If you already registered </Text>
-                    <Button style={{backgroundColor:"teal"}} onClick={handleLogin}>Login here</Button>
+                    <Button style={{backgroundColor:"teal"}} onClick={handleLogin}>Login</Button>
                 </Box>
             </form>
         </div>
